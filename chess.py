09729f68101d38f63bd755d4c7b0bdd055019f7e
orange5712 <107223548+orange5712@users.noreply.github.com>
2024-07-18 23:38:41 +0530

    Update chess.py
    Chess v1, bare-bones version with undefined movement, no checks or castling. Game only ends when enemy king is captured.

--- chess.py
+++ chess.py
@@ -1,29 +1,19 @@
-#board
-#peices
-#turns
-#move pieces 
-
-board= [9]*8 #creates an array of eight 9s
+board= [9]*8 
+#creates an array of eight 9s
 
 for i in range(len(board)):
-    board[i]= ['  ']*8 #updates the array so each 9 is replaced by an array of eight _s
-
-
-# for row in board:#for every element array in board
-#     for col in row:#for every element in each element array of board 
-#         print(col,end = ' ')#print the _ followed by a space
-#     print("")#after you finish one column (one array) print a line space
+    board[i]= ['  ']*8 
+#updates the array so each 9 is replaced by an array of eight double spaces
     
-def prt_brd(board):#we can do it in lists and then use ennumerate to print it out
+def prt_brd(board):
      for i,j in enumerate(board):
          print(8-i,end=": ")
          for k in range(len(j)):
              print(j[k], end=" ") 
          print()
      print('   A  B  C  D  E  F  G  H')
-    
+#prints board with array 0 as row '8: ' down to array 7 as row '1: ' with columns from 0 to 7 being listed from A to H
 
-#placing the pieces
 
 Bpcs={
     'Bp':[(1,0),(1,1),(1,2),(1,3),(1,4),(1,5),(1,6),(1,7)],
@@ -42,6 +32,7 @@ Wpcs={
     'Wk':[(7,4)],
     'Wq':[(7,3)]    
 }
+#dictionaries noting down the initial placement of all the white and black pieces
 
 def put_pcs(board):
     for i,j in Bpcs.items():
@@ -50,25 +41,21 @@ def put_pcs(board):
     for k,l in Wpcs.items():
         for n in l:
             board[n[0]][n[1]]=k
+#puts the pieces on the board in their starting positions
             
 put_pcs(board)
 prt_brd(board)            
 
-#move counter
 
-mc=0
-
+mv_c=0
 def movecount(board):
-    global mc
-    mc +=1
-    if mc % 2 ==1:
+    global mv_c
+    mv_c +=1
+    if mv_c % 2 ==1:
         print('White to move!')
     else:
         print('Black to move!')
-
-
-
-#moving
+#switches turns between white and black
 
 L_N={
   'A':'0',
@@ -80,17 +67,44 @@ L_N={
   'G':'6',
   'H':'7'
 }
+#dictionary that maps chess column notation to array column notation
+
+WK=[7,4]
+BK=[0,4]
+#original places of the both the Kings
 
 def move(board):
     while(True):
         movecount(board)
         s=input('move peice from:')
         e=input('move piece to:')
+        
         w,x=(8-int(s[1])),int(L_N[s[0]])
         y,z=(8-int(e[1])),int(L_N[e[0]])
+        
+        global WK
+        global BK
+        
+        if WK==[w,x]:
+            WK=[y,z]
+        
+        if BK==[w,x]:
+            BK=[y,z]
+            
         board[y][z]=board[w][x]
         board[w][x]="  "
-        prt_brd(board)
         
+        if board[BK[0]][BK[1]]=='Bk' and board[WK[0]][WK[1]]=='Wk':
+            prt_brd(board)
+        elif board[BK[0]][BK[1]]!='Bk':
+            print('black has lost')
+            return
+        else:
+            print('white has lost')
+            return
+
+#implements a version of chess where each king has to be captured for the other side to win
+
+
 move(board)
         
